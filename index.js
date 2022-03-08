c7d8fa6a06356a97f62ed91a7728d68823b5f7cb
yao <yaoyaox023@gmail.com>
2022-03-08 11:17:45 +0800

    add v2

--- index.js
+++ index.js
@@ -13,188 +13,153 @@ const ctx1 = canvas1.getContext('2d');
 const canvas2 = document.getElementById('canvas2');
 const ctx2 = canvas2.getContext('2d');
 
-const bgImg = document.getElementById("background-image")
-let bgImgData;
+const videoElement = document.getElementById("process-video");
+videoElement.width = 480;
+videoElement.height = 320;
+canvas1.width = videoElement.width;
+canvas1.height = videoElement.height;
+canvas2.width = videoElement.width;
+canvas2.height = videoElement.height;
 
-// 播放本地摄像头流
-let videoElement;
+let bgImageData;
+
 
 joinRoomBtn.addEventListener("click", joinRoom);
 
 // 全局房间对象
 const myRoom = new QNRTC.TrackModeSession();
 
-// 如果此时枚举设备的操作完成，就更新页面设备列表
-if (QNRTC.deviceManager.deviceInfo) {
-    addDeviceToSelect(QNRTC.deviceManager.deviceInfo);
-}
-// 当检测到枚举完成或者设备列表更新的时候，更新页面设备列表
-QNRTC.deviceManager.on("device-update", deviceInfo => {
-    addDeviceToSelect(deviceInfo);
-});
-
-// 将枚举到的设备信息添加到页面上
-function addDeviceToSelect(deviceInfo) {
-    // 清空之前 select 下的元素，重新遍历添加
-    while (audioDeviceSelect.firstChild) {
-        audioDeviceSelect.removeChild(audioDeviceSelect.firstChild);
-    }
-    while (videoDeviceSelect.firstChild) {
-        videoDeviceSelect.removeChild(videoDeviceSelect.firstChild);
-    }
-
-    // 遍历每个设备，添加到页面上供用户选择
-    deviceInfo.forEach(info => {
-        const optionElement = document.createElement("option");
-        optionElement.value = info.deviceId;
-        optionElement.text = info.label;
-        if (info.kind === "audioinput") {
-            audioDeviceSelect.appendChild(optionElement);
-        } else if (info.kind === "videoinput") {
-            videoDeviceSelect.appendChild(optionElement);
-        }
-    });
-}
-
 async function joinRoom() {
-    // 从输入框中获取 roomToken
-    const roomToken = roomTokenInput.value;
-    try {
-        // 加入房间
-        const users = await myRoom.joinRoomWithToken(roomToken);
-        // 因为我们假设是一对一连麦，如果加入后发现房间人数超过就退出报错
-        // 实际上这里更好的做法是在 portal 上连麦应用中配置好房间人数上限
-        // 这样就不要在前端做检查了
-        if (users.length > 2) {
-            myRoom.leaveRoom();
-            alert("房间人数已满！");
-            return;
-        }
+	// 从输入框中获取 roomToken
+	const roomToken = roomTokenInput.value;
+	try {
+		// 加入房间
+		const users = await myRoom.joinRoomWithToken(roomToken);
+		// 因为我们假设是一对一连麦，如果加入后发现房间人数超过就退出报错
+		// 实际上这里更好的做法是在 portal 上连麦应用中配置好房间人数上限
+		// 这样就不要在前端做检查了
+		if (users.length > 2) {
+			myRoom.leaveRoom();
+			alert("房间人数已满！");
+			return;
+		}
 
-        // 订阅房间中已经存在的 tracks
-        subscribeTracks(myRoom.trackInfoList);
-    } catch (e) {
-        console.error(e);
-        alert(`加入房间失败！ErrorCode: ${e.code || ""}`);
-        return;
-    }
+		// 订阅房间中已经存在的 tracks
+		subscribeTracks(myRoom.trackInfoList);
+	} catch (e) {
+		console.error(e);
+		alert(`加入房间失败！ErrorCode: ${e.code || ""}`);
+		return;
+	}
 
-    // 监听房间中其他人发布的 Track，自动订阅它们
-    myRoom.on("track-add", (tracks) => {
-        subscribeTracks(tracks);
-    });
+	// 监听房间中其他人发布的 Track，自动订阅它们
+	myRoom.on("track-add", (tracks) => {
+		subscribeTracks(tracks);
+	});
 
-    // 自动发布
-    await publish();
+	// 自动发布
+	await publish();
 }
 
 async function publish() {
-    let tracks;
-    try {
-        // 通过用户在页面上指定的设备发起采集
-        // 也可以不指定设备，这样会由浏览器自动选择
-        tracks = await QNRTC.deviceManager.getLocalTracks({
-            video: {
-                enabled: true, deviceId: videoDeviceSelect.value, bitrate: 1000, width: 320, height: 240,
-            }, audio: {
-                enabled: true, deviceId: audioDeviceSelect.value,
-            },
-        });
-    } catch (e) {
-        console.error(e);
-        alert(`采集失败，请检查您的设备。ErrorCode: ${e.code}`);
-        return;
-    }
 
-    // 播放采集到视频 Track
-    for (const track of tracks) {
-        if (track.info.kind === "video") {
-            track.play(processContainer);
-        }
-    }
+	// 初始化背景数据
+	const bgImg = document.getElementById("background-image");
+	ctx1.drawImage(bgImg, 0, 0, bgImg.width, bgImg.height, 0, 0, canvas1.width, canvas2.height);
+	bgImgData = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
 
-    // 拿到播放的 video 标签
-    videoElement = processContainer.querySelector("video")
-    videoElement.width = 320
-    videoElement.height = 240
-    canvas1.width = videoElement.width
-    canvas1.height = videoElement.height
-    canvas2.width = videoElement.width
-    canvas2.height = videoElement.height
+	// 启动 ai 处理
+	perform(videoElement);
 
-    // 初始化背景数据
-    ctx1.drawImage(bgImg, 0, 0)
-    bgImgData = ctx1.getImageData(0, 0, canvas1.width, canvas1.height)
+	// 从处理结果中提取 track
+	const stream = canvas2.captureStream();
+	const qntrack = QNRTC.createCustomTrack(stream.getVideoTracks()[0]);
 
-    // 启动 ai 处理
-    videoElement.onplaying = () => {
-        loadBodyPix()
-    }
+	const audioTracks = await QNRTC.deviceManager.getLocalTracks({ audio: { enabled: true } });
 
-    // 从处理结果中提取 track
-    const stream = canvas2.captureStream()
-    const qntrack = QNRTC.createCustomTrack(stream.getVideoTracks()[0])
-
-    try {
-        // 发布创建的新 track
-        await myRoom.publish([qntrack]);
-    } catch (e) {
-        console.error(e);
-        alert(`发布失败，ErrorCode: ${e.code}`);
-    }
+	try {
+		// 发布创建的新 track
+		await myRoom.publish([...audioTracks, qntrack]);
+	} catch (e) {
+		console.error(e);
+		alert(`发布失败，ErrorCode: ${e.code}`);
+	}
 }
 
 function subscribeTracks(trackInfoList) {
-    // 批量订阅 tracks，并在页面上播放
-    myRoom.subscribe(trackInfoList.map(t => t.trackId)).then(tracks => {
-        for (const track of tracks) {
-            if (track.info.kind === "video") {
-                track.play(remoteVideo);
-            } else {
-                track.play(trackContainer);
-            }
-        }
-    })
+	// 批量订阅 tracks，并在页面上播放
+	myRoom.subscribe(trackInfoList.map(t => t.trackId)).then(tracks => {
+		for (const track of tracks) {
+			if (track.info.kind === "video") {
+				track.play(remoteVideo);
+			} else {
+				track.play(trackContainer);
+			}
+		}
+	});
 }
 
-let net;
+async function perform(videoElement) {
+	const webcam = await tf.data.webcam(videoElement);
+	const modelUrl = 'http://r3dg6y3l0.hd-bkt.clouddn.com/WebRTC/model.json';
+	const model = await tf.loadGraphModel(modelUrl);
 
-function loadBodyPix() {
-    let options = {
-        architecture: 'MobileNetV1', multiplier: 0.75, stride: 16, quantBytes: 2
-    }
-    bodyPix.load(options)
-        .then(n => {
-            net = n
-            perform()
-        })
-        .catch(err => console.log(err))
+	// Set initial recurrent state
+	let [r1i, r2i, r3i, r4i] = [tf.tensor(0.), tf.tensor(0.), tf.tensor(0.), tf.tensor(0.)];
+
+	// Set downsample ratio
+	const downsample_ratio = tf.tensor(0.5);
+
+	// Inference loop
+	while (true) {
+		await tf.nextFrame();
+		const img = await webcam.capture();
+		const src = tf.tidy(() => img.expandDims(0).div(255)); // normalize input
+		const [fgr, pha, r1o, r2o, r3o, r4o] = await model.executeAsync(
+			{ src, r1i, r2i, r3i, r4i, downsample_ratio }, // provide inputs
+			['fgr', 'pha', 'r1o', 'r2o', 'r3o', 'r4o']   // select outputs
+		);
+
+		drawMatte(fgr.clone(), pha.clone(), canvas2);
+
+		// Dispose old tensors.
+		tf.dispose([img, src, fgr, pha, r1i, r2i, r3i, r4i]);
+
+		// Update recurrent states.
+		[r1i, r2i, r3i, r4i] = [r1o, r2o, r3o, r4o];
+	}
 }
 
-async function perform() {
-    const segmentation = await net.segmentPerson(videoElement);
 
-    //canvas-1上绘制视频流
-    ctx1.drawImage(videoElement, 0, 0);
-    const frame = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
-    const length = frame.data.length;
-    const data = frame.data;
-    const data_mask = segmentation.data;
+async function drawMatte(fgr, pha, canvas) {
+	const rgba = tf.tidy(() => {
+		const rgb = (fgr !== null) ?
+			fgr.squeeze(0).mul(255).cast('int32') :
+			tf.fill([pha.shape[1], pha.shape[2], 3], 255, 'int32');
+		const a = (pha !== null) ?
+			pha.squeeze(0).mul(255).cast('int32') :
+			tf.fill([fgr.shape[1], fgr.shape[2], 1], 255, 'int32');
+		return tf.concat([rgb, a], -1);
+	});
+	fgr && fgr.dispose();
+	pha && pha.dispose();
+	const [height, width] = rgba.shape.slice(0, 2);
+	const pixelData = new Uint8ClampedArray(await rgba.data());
 
-    //根据person分割的mask图拿到视频流中person的前景画面
-    for (let i = 0; i < length; i += 4) {
-        let index = i / 4;
-        // 空白处填入背景图数据
-        if (data_mask[index] == 0) {
-            data[i] = bgImgData.data[i]
-            data[i+1] = bgImgData.data[i+1]
-            data[i+2] = bgImgData.data[i+2]
-            data[i+3] = bgImgData.data[i+3]
-        }
-    }
-
-    //把上面的person前景画面绘制到带有背景图片的canvas-2上
-    ctx2.putImageData(frame, 0, 0);
-    requestAnimationFrame(perform)
+	for (let i = 0; i < pixelData.length; i += 4) {
+		if (pixelData[i + 3] !== 255) {
+			pixelData[i] = bgImgData.data[i];
+			pixelData[i + 1] = bgImgData.data[i + 1];
+			pixelData[i + 2] = bgImgData.data[i + 2];
+			pixelData[i + 3] = bgImgData.data[i + 3];
+		}
+	}
+	const imageData = new ImageData(pixelData, width, height);
+	canvas.width = width;
+	canvas.height = height;
+	const ctx = canvas.getContext('2d');
+	ctx.putImageData(imageData, 0, 0);
+	rgba.dispose();
 }
+
 
